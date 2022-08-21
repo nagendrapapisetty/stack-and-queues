3a502fe09aeb9f675dc2f460558a9fb15c53f364
unknown <papisettynagendra>
2022-08-21 14:04:23 +0530

    Insert Elements In Queue

--- StackAndQueues.java
+++ StackAndQueues.java
@@ -1,19 +1,23 @@
 package com.bridgelabz;
 
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class StackAndQueues {
 
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(70);
-        stack.push(30);
-        stack.push(56);
-        while (!stack.isEmpty()) {
-            System.out.println(stack.peek());
-            stack.pop();
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(56);
+        queue.add(30);
+        queue.add(70);
+        while (!queue.isEmpty()) {
+            System.out.println(queue.peek());
+            queue.remove();
 
         }
     }
 }
+
 
